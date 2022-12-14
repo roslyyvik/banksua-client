58230f0e6df87ff8985fc2ccf4c0be677bd177b6
Viktor Rosvik <roslyyvik@gmail.com>
2022-12-14 17:16:31 +0200

    App/pages

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import Navbar from './components/Navbar'
 import useToggleTheme from './hooks/useToggleTheme'
 import TopButton from './ui/button/TopButton'
 
-import Home from './pages/Home'
-import About from './pages/About'
-import Error from './pages/Error'
-import BanksTable from './pages/BanksTable'
-import BankPage from './pages/BankPage'
-import Register from './pages/Register'
-import Login from './pages/Login'
-import ProtectedRoute from './pages/ProtectedRoute'
-import Dashboard from './pages/Dashboard'
-import Verify from './pages/Verify'
+import {
+  About,
+  BankPage,
+  BanksTable,
+  Home,
+  Error,
+  Register,
+  Login,
+  Verify,
+  Dashboard,
+  ProtectedRoute,
+} from './pages';
 
 const App = () => {
   const toggleTheme = useToggleTheme()
---
+++ src/pages/index.js
@@ -0,0 +1,24 @@
+import Home from './Home';
+import Error from './Error';
+import Register from './Register';
+import Login from './Login';
+import Verify from './Verify';
+import Dashboard from './Dashboard';
+import ProtectedRoute from './ProtectedRoute';
+// import ForgotPassword from './ForgotPassword';
+// import ResetPassword from './ResetPassword';
+import About from './About'
+import BankPage from './BankPage'
+import BanksTable from './BanksTable'
+export {
+  Home,
+  About,
+  BankPage,
+  BanksTable,
+  Error,
+  Register,
+  Login,
+  Verify,
+  Dashboard,
+  ProtectedRoute,
+};
