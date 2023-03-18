f0b4782ec6f8a520cfc8966de439b2842753fee9
Viktor Rosvik <roslyyvik@gmail.com>
2023-03-18 12:35:36 +0200

    change URK

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,3 +1,3 @@
-const url = 'http://localhost:5500';
-// const url = 'https://banksua-api.cyclic.app'
+// const url = 'http://localhost:5500';
+const url = 'https://banksua-api.cyclic.app'
 export default url;
