fd48a866567ba25aebd5d21924c48be1f6578763
Viktor Rosvik <roslyyvik@gmail.com>
2023-03-18 16:16:00 +0200

    remove url protected

--- src/pages/ProtectedRoute.js
+++ src/pages/ProtectedRoute.js
@@ -1,8 +1,6 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useGlobalContext } from "../context";
 
-import url from '../utils/url'
-
 const ProtectedRoute = () => {
   const { user } = useGlobalContext()
   if(!user) {
